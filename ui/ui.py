7e7df7b4ecf12bc34cf673a3e29943c41825c795
Hua Nguyen Khoi <memuopmapdit1234@gmail.com>
2026-08-29 16:58:05 +0800

    UPDATE: change css type for ui

--- ui/ui.py
+++ ui/ui.py
@@ -11,6 +11,7 @@ from libs.pce.utils import apply_instance
 @dataclass(slots=True)
 class CSS_StyleType: 
     transform: Transform = field(default_factory=Transform)
+    visible: bool = True
 
 class UserInterfaceType(GameObject):
     def __init__(self, 
@@ -20,18 +21,32 @@ class UserInterfaceType(GameObject):
                  animations = None, 
                  connections = None):
         super().__init__(surface, audios, animations, None, connections)
-        self.style = apply_instance(CSS_StyleType(), style)
+
+        self._style = apply_instance(CSS_StyleType(), style)            # Default css style of UI
+                                                                        # It helps restore default style after new states of UI is end
+                                                                        # Must not change anything in here
+        
+        self._modified_style = apply_instance(CSS_StyleType(), style)   # Modified css style of UI
+                                                                        # It save and can be changed to be suitable with the current state of UI
+                                                                        # Shouldd use it to apply all changes, eg hovered, pressed, etc, for UI
 
     # States machine
     # All states of a UI
-    def hovered(self): ...
-    def pressed(self): ...
-    def actived(self): ...
-    def enabled(self): ...
-    def disabled(self): ...
+    # Function render all different states of UI
+    def hovered(self, style: CSS_StyleType): 
+        self._modified_style = style
+    def pressed(self, style: CSS_StyleType): 
+        self._modified_style = style
+    def actived(self, style: CSS_StyleType): 
+        self._modified_style = style
+    def enabled(self, style: CSS_StyleType): 
+        self._modified_style = style
+    def disabled(self, style: CSS_StyleType): 
+        self._modified_style = style
 
     # Render
     # This function must be called in update func in UI
+    # Function render default state of UI
     def _render(self): ...
 
     # Update
