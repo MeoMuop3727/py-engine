62e52851e327f937ad30ec469c89196d7e31320f
Hua Nguyen Khoi <memuopmapdit1234@gmail.com>
2026-08-29 16:24:03 +0800

    DEV: new ui class updated, add ui.py and __init__.py

---
+++ ui/__init__.py
@@ -0,0 +1,1 @@
+from .ui import UserInterfaceType, CSS_StyleType
--- ui/ui.py
+++ ui/ui.py
@@ -1,12 +1,36 @@
-from typing import Optional
-from libs.pce.systems import GameObject, Transform
-from libs.pce.utils import apply_instance
+import pygame
+from dataclasses import dataclass, field
+from libs.pce.systems import (
+    GameObject, 
+    Transform
+)
+
+@dataclass(slots=True)
+class CSS_StyleType: 
+    transform: Transform = field(default_factory=Transform)
 
 class UserInterfaceType(GameObject):
     def __init__(self, 
-                 surface = None, 
-                 transform: Optional[Transform] = None,
+                 surface: pygame.Surface,
+                 style: CSS_StyleType,
                  audios = None, 
-                 animations = None):
-        super().__init__(surface, audios, animations, None, None)
-        self.transform = apply_instance(Transform, transform)
+                 animations = None, 
+                 connections = None):
+        super().__init__(surface, audios, animations, None, connections)
+        self.style = style
+
+    # States machine
+    # All states of a UI
+    def hovered(self): ...
+    def pressed(self): ...
+    def actived(self): ...
+    def enabled(self): ...
+    def disabled(self): ...
+
+    # Render
+    # This function must be called in update func in UI
+    def _render(self): ...
+
+    # Update
+    # This function must be called in update func in scene
+    def update(self): ...
